d4bd9e7538774218b78bb3c85f51e4cd30002160
Danny Tuppeny <danny@tuppeny.com>
2018-05-28 13:02:52 +0100

    Add basic support for icons
    These don't work; needs investigation...

--- src/extension.ts
+++ src/extension.ts
@@ -54,6 +54,7 @@ const HTML_MODE: vs.DocumentFilter[] = [{ language: "html", scheme: "file" }];
 const DART_PROJECT_LOADED = "dart-code:dartProjectLoaded";
 const FLUTTER_PROJECT_LOADED = "dart-code:flutterProjectLoaded";
 export const SERVICE_EXTENSION_CONTEXT_PREFIX = "dart-code:serviceExtension.";
+export let extensionPath: string = null;
 
 let analyzer: Analyzer;
 let flutterDaemon: FlutterDaemon;
@@ -83,6 +84,7 @@ export function activate(context: vs.ExtensionContext, isRestart: boolean = fals
 	showLintNames = config.showLintNames;
 	previousSettings = getSettingsThatRequireRestart();
 
+	extensionPath = context.extensionPath;
 	const extensionStartTime = new Date();
 	util.logTime();
 	const sdks = findSdks();
